64f2615eb22a0d23d62fe6da3e74446dfd6b9455
Miguel Marques <147090004+MiguelMarques15@users.noreply.github.com>
2026-05-12 20:19:27 -0300

    Atualização na tela do Administrador

--- Login/src/pages/pg_adm/index.jsx
+++ Login/src/pages/pg_adm/index.jsx
@@ -1,5 +1,4 @@
 import './style.css'
-
 import { IoMdHome } from "react-icons/io";
 import { FaBoxOpen , FaMapPin, FaRegBell, FaUser, FaHome, FaDonate } from "react-icons/fa";
 import { GoAlertFill } from "react-icons/go";
@@ -7,62 +6,59 @@ import { FaHeart, FaGear } from "react-icons/fa6";
 
 const PgAdm = () => {
   return (
-    <div class="dashboard">
+    
+    <div className="dashboard">
  
       <aside class="sidebar">
         <ul>
-          <li class="active"><FaHome className= "icon" /> Home</li>
+          <li classNme="active"><FaHome className= "icon" /> Home</li>
           <li><FaBoxOpen  className= "icon" /> Abrigos</li>
-          <li>< FaDonate className= "icon" /> Doações</li>
-          <li><FaMapPin className= "icon" />Regiões Afetadas</li>
+          <li><FaDonate className= "icon" /> Doações</li>
+          <li><FaMapPin className= "icon" />Região Afetada</li>
           <li><GoAlertFill className= "icon" /> Ocorrências</li>
-          <li><FaHeart className= "icon" /> Necessidades</li>
-          <li><FaGear className= "icon" /> Configurações</li>
+          <li><FaHeart className= "icon" /> ?</li>
+          <li><FaUser className= "icon" /> Perfil</li>
         </ul>
       </aside>
 
 
-      <main class="main">
+      <main className="main">
 
         
-        <header class="top">
-          <h1>Olá, Administrador(a)!</h1>
+        <header className="top">
+          <p>Olá, Administrador(a)!</p>
           <div class="top-icons">
-            <FaRegBell className= "icon2" /> 
-            <FaUser className= "icon3" /> 
            <img src="src/assets/logo.png" width= "80px" />
           </div>
         </header>
 
-
-        <section class="alerts">
+        <div className="text-alert">  
           <h3>Alertas Recentes</h3>
-
+        </div>
+        <section className="alerts">
           <div class="alert">
             ⚠️ Níveis de água estão subindo no canal de drenagem de Caçapava
             <span>20min</span>
           </div>
 
-          <div class="alert">
+          <div className="alert">
             ⚠️ Princípios de queimadas na região de Taubaté
             <span>50min</span>
           </div>
 
-          <div class="alert">
+          <div className="alert">
             ⚠️ Deslizamento na rodovia Osvaldo Cruz
             <span>2 horas</span>
           </div>
         </section>
 
-
-        <section class="actions">
-          <h3>Ações Rápidas</h3>
-
-          <div class="cards">
-            <div class="card"><img src="src/assets/doacao.jpeg" /><p>Registrar Doação</p></div>
-            <div class="card"><img src="src/assets/regiao.jpeg" /><p>Atualizar Regiões</p></div>
-            <div class="card"><img src="src/assets/user.jpeg" /><p>Visualizar Municípios</p></div>
-            <div class="card"><img src="src/assets/voluntario.jpeg" /><p>Visualizar Voluntários</p></div>
+        <div className="text-acoes"><h3>Ações Rápidas</h3></div>
+        <section className="actions">
+          <div className="cards">
+            <div className="card"><img src="src/assets/doacao.jpeg" /><p>Registrar Doação</p></div>
+            <div className="card"><img src="src/assets/regiao.jpeg" /><p>Atualizar Regiões</p></div>
+            <div className="card"><img src="src/assets/user.jpeg" /><p>Visualizar Municípios</p></div>
+            <div className="card"><img src="src/assets/voluntario.jpeg" /><p>Visualizar Voluntários</p></div>
           </div>
         </section>
 
